9d2dcaca24a93aba0c934d28194b51396a90fbf1
Ricardo Camarena <ricky_camarena@hotmail.com>
2021-10-14 20:13:26 -0500

    Prueba de adicion de diccionarios

--- prueba1.py
+++ prueba1.py
@@ -3,16 +3,19 @@ f = open('test1.txt','r')
 message = f.read()
 
 # split data
-generalData = message.splitlines()       
-print(generalData)       
+generalData = message.splitlines()
+print(generalData)
 states = generalData[0].split(',')       #['q0', 'q1', 'q2', 'q3']
 alphabet  = generalData[1].split(',')    #['a', 'b']
 initialState  = generalData[2]           #q0
 finalState  = generalData[3].split(',')  #['q3']
 
 for x in range(4, len(generalData)):
-  print(generalData[x]) 
-#   agregar datos a diccionario 
+  print(generalData[x])
+  #Separar generalData en caracteres
 
+  #agregar datos a diccionario
+  transitionTable = {states[0] : ["q1","q2","q3"]}
 
 f.close()
+print(transitionTable)
